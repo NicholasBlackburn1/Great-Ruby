2a6973fb0d5f6e62495478fa1fd0c2f0e4aece3f
BuildTools <unconfigured@null.spigotmc.org>
2018-12-03 10:14:14 -0500

    added obfs

--- obfs.rb
+++ obfs.rb
@@ -3,11 +3,15 @@ Obfuscate.setup do |config|
     config.salt = "A weak salt ..." # Length must be between 1-56 
     config.mode = :string           # defaults to :string
   end
-  puts("--Great-Ruby--")
-  puts("A text obfucator")
+  puts("--Great-Ruby--\n")
+  puts("A text obfucator\n")
+  puts ("Enter a String")
   text= gets
   obfuscated = Obfuscate.obfuscate( text )
   clarified = Obfuscate.clarify( obfuscated ) 
   print obfuscated
-  puts("you wana")
-  
+  puts("you wana see the hidded message\n")
+  num = gets 
+  if (num == 1)
+    put(clarified)
+  end
